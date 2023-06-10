a320f2a0bd5569a55344c10e3938463056902750
blainefreestone <blainefreestone01@gmail.com>
2023-06-10 07:53:43 -0700

    Magic number game added.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userNumber = -1;
+
+        while (userNumber != magicNumber)
+        {
+            Console.WriteLine("What is the magic number? ");
+            string userNumberInText = Console.ReadLine();
+            userNumber = int.Parse(userNumberInText);
+
+            if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
