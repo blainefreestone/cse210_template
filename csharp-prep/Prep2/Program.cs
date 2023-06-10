66fadf56a0ca57c869d10f4f4e636fd8ab94f531
blainefreestone <blainefreestone01@gmail.com>
2023-06-09 18:20:28 -0700

    Grade percentage calculator added.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradeInText = Console.ReadLine();
+        int grade = int.Parse(gradeInText);
+
+        if (grade >= 90) 
+        {
+            string letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            string letter = "A";
+        }
+        else if (grade >= 70)
+        {
+            string letter = "A";
+        }
+        else if (grade >= 60)
+        {
+            string letter = "A";
+        }
+        else
+        {
+            string letter = "A";
+        }
+
+        Console.WriteLine($"{letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class! Congratulations!!!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class :( Better luck next time!!!");
+        }
     }
 }
