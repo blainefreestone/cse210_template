2fe817b681505c3ddfeca1887fca3c7055aa7967
blainefreestone <blainefreestone01@gmail.com>
2023-06-10 08:30:11 -0700

    Functions and main function added.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string numberInText = Console.ReadLine();
+        int number = int.Parse(numberInText);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        number = SquareNumber(number);
+        DisplayResult(name, number);
+
     }
 }
