5e50c937184e8820c630e0e7f2f6829a04cfcb60
blainefreestone <blainefreestone01@gmail.com>
2023-06-26 12:26:16 -0700

    Changed studentName to protected.

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -6,7 +6,7 @@ public class Assignment
         _topic = topic;
     }
     
-    private string _studentName;
+    protected string _studentName;
     private string _topic;
     public string GetSummary()
     {
