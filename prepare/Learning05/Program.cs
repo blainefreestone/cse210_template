c2532965432618c54c719bb22590796c1d9d9e65
blainefreestone <blainefreestone01@gmail.com>
2023-07-06 16:10:07 -0700

    List of shapes and iteration through them added.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("orange", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("yellow", 2, 4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("green", 3.4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}; Area: {shape.GetArea()}");
+        }
     }
 }
