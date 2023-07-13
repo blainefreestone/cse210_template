739560440e1859a4e6c49885362d13e8ccc12e57
blainefreestone <blainefreestone01@gmail.com>
2023-07-13 16:04:36 -0700

    various changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,29 @@
-using System;
+using System.Windows.Forms;
 
-class Program
+namespace myform
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Sandbox World!");
+        static void Main(string[] args)
+        {
+            Form myform = new Form();
+            Button mybutton = new Button()
+            {
+                Text = "Hello",
+                Location = new System.Drawing.Point(10, 10)
+            };
+            mybutton.Click += (o, s) =>
+            {
+                MessageBox.Show("world");
+            };
+
+            myform.Controls.Add(mybutton);
+            myform.ShowDialog();
+
+            while (myform.Created)
+            {
+
+            }
+        }
     }
 }
